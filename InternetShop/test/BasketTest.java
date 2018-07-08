dec276ed21874319aeb8866ac2ed26869cea7a3a
sedziej <sedziej@tlen.pl>
2018-07-08 22:23:23 +0200

    more junit cases

--- InternetShop/test/BasketTest.java
+++ InternetShop/test/BasketTest.java
@@ -15,8 +15,9 @@ public class BasketTest {
 	
 	@After
 	public void tearDown() {
-		
+		basket = null;
 	}
+	
 	@Test
 	public void testConstructorOfBasket() {
 		assertTrue(basket != null);
@@ -29,6 +30,16 @@ public class BasketTest {
 		assertTrue(basket.getItem(testItem.getName()) != null);
 	}
 	
+	@Test 
+	public void testAddItemsWithSameNames() {	
+		String testItemName = "Cheap item";
+		Item testItem1 = new Item(testItemName, 1.0);
+		Item testItem2 = new Item(testItemName, 1.0);
+		
+		basket.addItem(testItem1.getName(), testItem1.getPrice());
+		basket.addItem(testItem2.getName(), testItem2.getPrice());
+		assertTrue(basket.getItem(testItemName).getCount() > 1);
+	}
 	@Test
 	public void testRemoveItem() {
 		fail("Not yet implemented");
